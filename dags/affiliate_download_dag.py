8a242588b3a62326700e9e47b203cbd78fad9936
phu.ph <newlight.9x@gmail.com>
2025-06-22 23:25:10 +0700

    update

--- dags/affiliate_download_dag.py
+++ dags/affiliate_download_dag.py
@@ -6,18 +6,18 @@ import asyncio
 import os
 
 from utils.affiliate_connector import InvolveAsyncConnector
-from utils.exporter import export_involve_conversion_to_csv
+from utils.storage_exporter import CsvExporter
+from utils.storage_connector import NocodbConnector
 
-
-def download_involve_asia_data(**context):
-    secret_key = Variable.get("involve_asia_secret_key_1")
-    output_path = os.path.join(os.path.dirname(__file__), "/opt/airflow/data/affiliate_data.csv")
-    state_key = "involve_asia_downloaded_once"
+def download_and_export_csv_affiliate_data(**context):
+    secret_key = Variable.get("affiliate_secret_key")
+    output_path = os.path.join(os.path.dirname(__file__), "../data/affiliate_data.csv")
+    state_key = "affiliate_downloaded_once"
 
     # Kiểm tra trạng thái đã từng chạy chưa
     downloaded_once = Variable.get(state_key, default_var="0") == "1"
 
-    async def fetch_and_save():
+    async def fetch_all_data():
         connector = InvolveAsyncConnector(secret_key=secret_key)
         await connector.authenticate()
         all_data = []
@@ -41,22 +41,64 @@ def download_involve_asia_data(**context):
                 if not has_next:
                     break
                 page += 1
-        export_involve_conversion_to_csv(output_path, all_data)
+        return all_data
 
-    asyncio.run(fetch_and_save())
+    def export_csv(data):
+        CsvExporter(filepath=output_path).export(data)
 
+    all_data = asyncio.run(fetch_all_data())
+    export_csv(all_data)
+
+def download_and_export_nocodb_affiliate_data(**context):
+    secret_key = Variable.get("involve_asisa_secret_key_1")
+    nocodb_api_url = Variable.get("nocodb_api_url")
+    nocodb_token = Variable.get("nocodb_token")
+    state_key = "affiliate_downloaded_once"
+
+    downloaded_once = Variable.get(state_key, default_var="0") == "1"
+
+    async def fetch_all_data():
+        connector = InvolveAsyncConnector(secret_key=secret_key)
+        await connector.authenticate()
+        all_data = []
+        page = 1
+        limit = 100
+        if not downloaded_once:
+            while True:
+                data, has_next = await connector.get_conversion(page=str(page), limit=str(limit))
+                all_data.extend(data)
+                if not has_next:
+                    break
+                page += 1
+            Variable.set(state_key, "1")
+        else:
+            yesterday = (datetime.now() - timedelta(days=1)).strftime("%Y-%m-%d")
+            while True:
+                data, has_next = await connector.get_conversion(start_date=yesterday, end_date=yesterday, page=str(page), limit=str(limit))
+                all_data.extend(data)
+                if not has_next:
+                    break
+                page += 1
+        return all_data
+
+    all_data = asyncio.run(fetch_all_data())
+    connector = NocodbConnector(api_url=nocodb_api_url, token=nocodb_token)
+    connector.push(all_data)
 
 with DAG(
-    dag_display_name="[ADS] - Download report data. ",
-    dag_id="ads_aff_download_data",
+    dag_id="affiliate_download_dag",
     start_date=datetime(2024, 1, 1),
     schedule_interval=None,
     catchup=False,
-    tags=["ads"],
+    tags=["affiliate"],
 ) as dag:
-    download_task = PythonOperator(
-        task_display_name="Download InvolveAsia data",
-        task_id="download_involve_asia_acc_1_data",
-        python_callable=download_involve_asia_data,
+    # download_task = PythonOperator(
+    #     task_id="download_affiliate_data",
+    #     python_callable=download_and_export_csv_affiliate_data,
+    #     provide_context=True,
+    # )
+    export_to_nocodb_task = PythonOperator(
+        task_id="push_affiliate_to_nocodb",
+        python_callable=download_and_export_nocodb_affiliate_data,
         provide_context=True,
     ) 
---
+++ utils/storage_exporter.py
@@ -0,0 +1,53 @@
+import httpx
+import math
+import asyncio
+import csv
+import os
+
+
+class StorageExporter:
+    def __init__(self, **config):
+        pass
+
+    def export(self, data, **settings):
+        pass
+
+
+class NocodbExporter(StorageExporter):
+    def __init__(self, **config):
+        super().__init__()
+        self._api_url = config.get('api_url')
+        self._token = config.get('token')
+
+    def export(self, data, **settings):
+        batch_size = settings.get('batch_size', 1000)
+        headers = {
+            "accept": "application/json",
+            "xc-token": self._token,
+            "Content-Type": "application/json"
+        }
+        responses = []
+        total = len(data)
+        for i in range(0, total, batch_size):
+            batch = data[i:i+batch_size]
+            resp = httpx.post(self._api_url, headers=headers, json=batch, timeout=30.0)
+            resp.raise_for_status()
+            responses.append(resp.json())
+        return responses
+
+
+class CsvExporter:
+    def __init__(self, **config):
+        super().__init__()
+        self._filepath = config.get('filepath')
+
+    def export(self, data, **settings):
+        os.makedirs(os.path.dirname(self._filepath), exist_ok=True)
+        if not data or not isinstance(data, list):
+            raise ValueError("Data must be a list of dicts")
+        if len(data) == 0:
+            return
+        with open(filepath, mode='w', newline='', encoding='utf-8') as f:
+            writer = csv.DictWriter(f, fieldnames=data[0].keys())
+            writer.writeheader()
+            writer.writerows(data)
